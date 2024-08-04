db1e40905a00c4359b4bcc92036d9db8f0bd4cee
taromn <54003207+taromn@users.noreply.github.com>
2024-08-04 14:41:08 +0000

    minor comment

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -28,5 +28,10 @@ func main() {
 }
 
 // what is Addr?
-// Addr is passed to net.Listen() function
+// Addr is a string passed to net.Listen() function
 // https://github.com/golang/go/blob/c7ea20195a3415668047eebdc488a4af1f629f04/src/net/http/server.go#L3247-L3255
+
+
+// ln, err := net.Listen("tcp", ":8080")
+
+// net.Listen returns net.TCPconn that has Read Write methods.
